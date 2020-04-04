57b0a48d5ceb39b17f4b12a470f9c232ca7ec558
Dilshan <dopatha@nc.rr.com>
2020-04-04 00:25:23 -0400

    fixed nav bar

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 window.onload=function() {
     var startButton = document.getElementById("startbtn")
     startButton.addEventListener("click", function(){
-        setTime();
+        setTime(120);
         setQuestion();
         console.log(event);
     });
   }
+
+  
+  //test code
+
+<div class="hide" id="divOne"></div>
+<div id="divTwo"></div>
+<div id="divThree"></div>
+<div id ="questionDiv">
+    all the html from line 21
+</div>
+
+
+document.querySelector("#questionDiv").appendChild('<div class="hide" id="divOne"></div><div id="divTwo"></div><div id="divThree"></div>');
+
+document.querySelector("#startButton").addEventListener("click",function(){
+
+})
+
+
+document.querySelector(".hide").classList.remove("hide")
+
+.hide {
+    display:none;
+}
+
+
+
+  //end test code
 var timeElement = document.querySelector("<p>");
 var secondsLeft = 120;
 function setTime() {
