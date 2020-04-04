24312b5821f9fc268f1a38e96d3431aedb44c55a
Dilshan <dopatha@nc.rr.com>
2020-04-04 18:06:36 -0400

    hiding id's

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-window.onload=function() {
-    var startButton = document.getElementById("startbtn")
-    startButton.addEventListener("click", function(){
-        setTime();
-        setQuestion();
-        console.log(event);
-    });
-  }
+var button = document.querySelector('#startbtn');
+var timer = 120;
+var timeText = document.querySelector('#timer');
+
+
+document.getElementById('questions').style.display = 'none'
+document.getElementById('answer').style.display = 'none'
+document.getElementById('input').style.display = 'none'
+document.getElementById('table').style.display = 'none'
+
+
+
+button.onclick = startQuiz;
+
+function startQuiz() {
+    setInterval(function() {
+    timer--
+    document.querySelector('#timer').innerHTML = timer 
+  }, 1000);
+
+  document.getElementById('Header').style.display = 'none'
+}
+  document.querySelector('#timer').innerHTML = timer
+
+
+
+
+
+
+
+
+// window.onload=function() {
+//     var startButton = document.getElementById("startbtn")
+//     startButton.addEventListener("click", function(){
+//         setTime();
+//         setQuestion();
+//         console.log(event);
+//     });
+//   }
 
   
   //test code
@@ -34,21 +65,21 @@ document.querySelector(".hide").classList.remove("hide")
 
 
   //end test code
-var timeElement = document.querySelector("<p>");
-var secondsLeft = 120;
-function setTime() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeElement.textContent = secondsLeft
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            sendTimeMessage();
+// var timeElement = document.querySelector("<p>");
+// var secondsLeft = 120;
+// function setTime() {
+//     var timerInterval = setInterval(function() {
+//         secondsLeft--;
+//         timeElement.textContent = secondsLeft
+//         if (secondsLeft === 0) {
+//             clearInterval(timerInterval);
+//             sendTimeMessage();
             
-        }
-    }, 1000);
-}
-function sendTimeMessage() {
-    timeElement.textContent = " ";
-}
-function setQuestion() {
-}
+//         }
+//     }, 1000);
+// }
+// function sendTimeMessage() {
+//     timeElement.textContent = " ";
+// }
+// function setQuestion() {
+// }
